b6cf4cbc5e0a63ced9dea176253ba50d4b29c22d
Eduardo Lozano <eduardoellozi2023@gmail.com>
2024-09-12 11:07:16 -0300

    criando contexto para conexao do rabbit e injetando no producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Services.Context;
+
+public class Program {
+    public static void Main(string[] args) {
+        CreateHostBuilder(args).Build();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) => {
+                services.AddSingleton<RabbitContext>();
+            });
+}
